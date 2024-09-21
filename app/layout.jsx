6cd330b92f67e37335ed23e40eee1907093ca9e0
zotel1 <zotelsigel@gmail.com>
2024-09-20 23:58:47 -0300

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,7 @@
-import localFont from "next/font/local";
+import localFont, { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin" ]});
 
 export const metadata = {
   title: "Create Next App",
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+        center: true,
+        padding: "15px",
+    },
+    screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '960px',
+        xl: '1200px',
+    },
   	extend: {
   		colors: {
   			background: 'var(--background)',
