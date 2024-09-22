9406431db515a647bdf8badaf25d06c9d17fcd3f
zotel1 <zotelsigel@gmail.com>
2024-09-21 22:00:01 -0300

    update nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,3 +1,27 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+    {
+        name: "home",
+        path: "/",
+    },
+    {
+        name: "home",
+        path: "/",
+    },
+    {
+        name: "home",
+        path: "/",
+    },
+    {
+        name: "home",
+        path: "/",
+    },
+]
+
 const Nav = () => {
     return <nav>desktop nav</nav>
 };
